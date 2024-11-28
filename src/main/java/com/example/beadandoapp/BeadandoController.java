6b80fe84547928aec301ffa3ab3342cefb37913c
leventemunkh <leventem96@gmail.com>
2024-11-28 20:25:43 +0100

    Adatbázis és menü hozzáadva
    Adatbázis-műveletek metódusai a **BeadandoController**-ben, menüstruktúra a **hello-view.fxml**-ben, SQLite JDBC a **pom.xml**-hez, és **DataBaseHelper** osztály létrehozva. Git verziókezelés beállítva.

--- src/main/java/com/example/beadandoapp/BeadandoController.java
+++ src/main/java/com/example/beadandoapp/BeadandoController.java
@@ -1,14 +1,63 @@
 package com.example.beadandoapp;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class BeadandoController {
+
+    // Adatbázis menü
     @FXML
-    private Label welcomeText;
+    protected void onOlvasClick() {
+        System.out.println("Adatbázis Olvas menüpont aktiválva!");
+        // Itt megjeleníthetsz adatokat egy táblázatban.
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onIrClick() {
+        System.out.println("Adatbázis Ír menüpont aktiválva!");
+        // Itt hozzáadhatsz új rekordokat.
+    }
+
+    @FXML
+    protected void onModositClick() {
+        System.out.println("Adatbázis Módosít menüpont aktiválva!");
+        // Itt módosíthatsz meglévő adatokat.
+    }
+
+    @FXML
+    protected void onTorolClick() {
+        System.out.println("Adatbázis Töröl menüpont aktiválva!");
+        // Itt törölhetsz rekordokat.
+    }
+
+    // SOAP Kliens menü
+    @FXML
+    protected void onLetoltesClick() {
+        System.out.println("SOAP Letöltés menüpont aktiválva!");
+        // Itt letöltheted az adatokat.
+    }
+
+    @FXML
+    protected void onGrafikonClick() {
+        System.out.println("SOAP Grafikon menüpont aktiválva!");
+        // Itt grafikont jeleníthetsz meg.
+    }
+
+    // Forex menü
+    @FXML
+    protected void onSzamlaInfoClick() {
+        System.out.println("Forex Számlainformációk menüpont aktiválva!");
+        // Itt számlainformációkat jeleníthetsz meg.
+    }
+
+    @FXML
+    protected void onArakClick() {
+        System.out.println("Forex Aktuális árak menüpont aktiválva!");
+        // Itt aktuális árakat kérdezhetsz le.
+    }
+
+    @FXML
+    protected void onHistorikusClick() {
+        System.out.println("Forex Historikus árak menüpont aktiválva!");
+        // Itt historikus adatokat kérdezhetsz le.
     }
 }
---
+++ src/main/java/com/example/beadandoapp/DataBaseHelper.java
@@ -0,0 +1,4 @@
+package com.example.beadandoapp;
+
+public class DataBaseHelper {
+}
