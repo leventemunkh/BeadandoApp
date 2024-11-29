0ba650215b250655230c76dfdafb73dacec79cf5
leventemunkh <leventem96@gmail.com>
2024-11-29 19:15:28 +0100

    SOAP implementálása

--- src/main/java/com/example/beadandoapp/BeadandoController.java
+++ src/main/java/com/example/beadandoapp/BeadandoController.java
@@ -3,11 +3,24 @@ package com.example.beadandoapp;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
-
 public class BeadandoController {
+
+    @FXML
+    private TextField dateField;
+
+    @FXML
+    private ComboBox<String> currencyComboBox;
+
+    @FXML
+    private CheckBox activeOnlyCheckBox;
 
     @FXML
     protected void onOlvasClick() {
@@ -41,8 +54,6 @@ public class BeadandoController {
         }
     }
 
-
-
     @FXML
     protected void onIrClick() {
         System.out.println("Adatbázis Ír menüpont aktiválva!");
@@ -59,7 +70,6 @@ public class BeadandoController {
         }
     }
 
-
     @FXML
     protected void onModositClick() {
         System.out.println("Adatbázis Módosít menüpont aktiválva!");
@@ -75,7 +85,6 @@ public class BeadandoController {
             System.err.println("Hiba a Módosít nézet betöltésekor: " + e.getMessage());
         }
     }
-
 
     @FXML
     protected void onTorolClick() {
@@ -95,16 +104,52 @@ public class BeadandoController {
 
     // SOAP Kliens menüpontok
     @FXML
-    protected void onLetoltesClick() {
-        System.out.println("SOAP Letöltés menüpont aktiválva!");
-        // Itt lehet majd adatokat letölteni a SOAP klienssel
+    protected void onSoapDownloadClick() {
+        System.out.println("SOAP Letöltés aktiválva!");
+        try {
+            SOAPClient.downloadAllData("MNB.txt");
+            System.out.println("Adatok sikeresen letöltve az MNB.txt fájlba.");
+        } catch (Exception e) {
+            System.err.println("Hiba a letöltés során: " + e.getMessage());
+        }
+    }
+
+
+    @FXML
+    protected void onSoapDownloadFilteredClick() {
+        System.out.println("SOAP Letöltés2 aktiválva!");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("soap-filter-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setTitle("SOAP Szűrt Letöltés");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("Hiba a SOAP Szűrt Letöltés nézet betöltésekor: " + e.getMessage());
+        }
     }
 
     @FXML
-    protected void onGrafikonClick() {
-        System.out.println("SOAP Grafikon menüpont aktiválva!");
-        // Itt lehet majd egy grafikont megjeleníteni a letöltött adatok alapján
+    protected void onFilterDownload() {
+        String date = dateField.getText();
+        String currency = currencyComboBox.getValue();
+        boolean activeOnly = activeOnlyCheckBox.isSelected();
+
+        System.out.println("Szűrő beállítások:");
+        System.out.println("Dátum: " + date);
+        System.out.println("Valuta: " + currency);
+        System.out.println("Csak aktív: " + activeOnly);
+
+        try {
+            SOAPClient.downloadFilteredData("MNB.txt", date, currency, activeOnly);
+            System.out.println("Szűrt adatok sikeresen letöltve az MNB.txt fájlba.");
+        } catch (Exception e) {
+            System.err.println("Hiba a szűrt letöltés során: " + e.getMessage());
+        }
     }
+
+
 
     // Forex menüpontok
     @FXML
@@ -124,7 +169,4 @@ public class BeadandoController {
         System.out.println("Forex Historikus árak menüpont aktiválva!");
         // Itt lehet majd historikus árakat megjeleníteni
     }
-
-
-
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,10 @@ module com.example.beadandoapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires com.sun.xml.ws;
+    requires java.xml.bind;
+    requires java.desktop;
+    requires jakarta.activation;
 
 
     opens com.example.beadandoapp to javafx.fxml;
